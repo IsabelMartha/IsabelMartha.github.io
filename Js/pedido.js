41bfeb1fd56bbe532b89642c61975ad815f484bc
MarthaCallejo <114033713+IsabelMartha@users.noreply.github.com>
2023-09-15 23:49:47 -0300

    productos agregados

--- Js/pedido.js
+++ Js/pedido.js
@@ -1,9 +1,168 @@
 function getProductos(){
     return [{
-        img:' <img class="card-img-top img-background" src="../Images/capalluvia-min.jpg" alt="piloto">',
-        img:' <img class="card-img-top img-background" src="../Images/capalluvia-min.jpg" alt="piloto">',
+        img:'../Images/capalluvia-min.jpg',
         title:'Piloto',
         description:'Proteje a tu perro con la mejor capa',
+        price:'$50'
+    }, {
+        img:'../Images/chaquetaperro-min.jpg',
+        title:'Chaqueta',
+        description:'Con wata termica',
+        price:'$50'
+    }, {
+        img:'../Images/camaperro-min.jpg',
+        title:'Cama',
+        description:'UltraSoft',
+        price:'$50'
+    }, {
+        img:'../Images/camaperro2-min.jpg',
+        title:'Cama',
+        description:'UltraSoft',
+        price:'$50'
+    }, {
+        img:'../Images/camaperro3-min.jpg',
+        title:'Cama',
+        description:'Con wata termica',
+        price:'$50'
+    }, {
+        img:'../Images/camaperro4-min.jpg',
+        title:'Cama',
+        description:'UltraSoft',
+        price:'$50'
+    }, {
+        img:'../Images/casaperro-min.jpg',
+        title:'Casita',
+        description:'100% pvc',
+        price:'$50'
+    }, {
+        img:'../Images/casaperro1-min.jpg',
+        title:'Casita',
+        description:'Madera de Pino',
+        price:'$50'
+    }, {
+        img:'../Images/chaqueta2perro-min.jpg',
+        title:'Chaqueta',
+        description:'Con guata',
+        price:'$50'
+    }, {
+        img:'../Images/comedorperro-min.jpg',
+        title:'Comedor',
+        description:'Con plato automatico',
+        price:'$50'
+    }, {
+        img:'../Images/corbatinperro-min.jpg',
+        title:'Corbatin',
+        description:'100% algodon',
+        price:'$50'
+    }, {
+        img:'../Images/jugueteperro-min.jpg',
+        title:'Kit de juguetes',
+        description:'Incluye una cuerda',
+        price:'$50'
+    }, {
+        img:'../Images/kitpeluqueria-min.jpg',
+        title:'Kit Peluqueria',
+        description:'La mejor opción',
+        price:'$50'
+    }, {
+        img:'../Images/transportadorperro-min.jpg',
+        title:'Canil',
+        description:'Razas pequeñas',
+        price:'$50'
+    }, {
+        img:'../Images/portabolsas-min.jpg',
+        title:'Portabolsas',
+        description:'Biodegradable',
+        price:'$50'
+    }, {
+        img:'../Images/comedorgato-min.jpg',
+        title:'Platos',
+        description:'Plato y bebedero',
+        price:'$50'
+    }, {
+        img:'../Images/cortauñasgato-min.jpg',
+        title:'Corta Uñas',
+        description:'Acero inoxidable',
+        price:'$50'
+    }, {
+        img:'../Images/hiervagato-min.jpg',
+        title:'Hierva para gato',
+        description:'No toxica',
+        price:'$50'
+    }, {
+        img:'../Images/jugueteHuppy-min.jpg',
+        title:'Platillos',
+        description:'Plato con pelotas',
+        price:'$50'
+    }, {
+        img:'../Images/ratoncitos-min.jpg',
+        title:'Ratoncitos',
+        description:'Algodon',
+        price:'$50'
+    }, {
+        img:'../Images/torregato-min.jpg',
+        title:'Torre',
+        description:'Plegable',
+        price:'$50'
+    }, {
+        img:'../Images/transportingato-min.jpg',
+        title:'Transportin',
+        description:'Hasta 10kg',
+        price:'$50'
+    }, {
+        img:'../Images/kitgato-min.jpg',
+        title:'Kit',
+        description:'Kit higienico',
+        price:'$50'
+    }, {
+        img:'../Images/varita-min.jpg',
+        title:'Varita',
+        description:'Con plumas',
+        price:'$50'
+    }, {
+        img:'../Images/conejokit-min.jpg',
+        title:'Kit para Conejo',
+        description:'Plato de acero inoxidable',
+        price:'$50'
+    }, {
+        img:'../Images/ruedahamster-min.jpg',
+        title:'Rueda',
+        description:'Varios colores',
+        price:'$50'
+    }, {
+        img:'../Images/kitbasicopecera-min.jpg',
+        title:'Kit para Pecera',
+        description:'Con red limpiadora',
+        price:'$50'
+    }, {
+        img:'../Images/pecheraconejo-min.jpeg',
+        title:'Pechera',
+        description:'Correa Ajustables',
+        price:'$50'
+    }, {
+        img:'../Images/conejoalim-min.jpg',
+        title:'Alimentador',
+        description:'PVC',
+        price:'$50'
+    }, {
+        img:'../Images/jaulahamster-min.jpg',
+        title:'Jaula para Hamster',
+        description:'No incluye laberinto',
+        price:'$50'
+    }, {
+        img:'../Images/jaulaconejo-min.jpg',
+        title:'Jaula para Conejo',
+        description:'Incluye bandeja de plastico ',
+        price:'$50'
+    }, {
+        img:'../Images/peceradeco-min.jpg',
+        title:'Acc para peceras',
+        description:'Varios modelos',
+        price:'$50'
+    }, {
+        img:'../Images/kittijeras-min.jpg',
+        title:'Kit',
+        description:'Alicate y lima',
         price:'$50'
     }];
 }
--- Js/productos.js
+++ Js/productos.js
@@ -1,7 +1,6 @@
 class Producto {
     constructor(producto){
         this.img = producto.img;
-        this.img = producto.img;
         this.title = producto.title;
         this.description = producto.description;
         this.price = producto.price;
