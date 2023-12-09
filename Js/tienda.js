bfb8228575109fbc8a0cf5028afaba1fb3703fa1
julieta mores <julieta.mores@itscordoba.edu.ar>
2023-12-09 20:54:58 -0300

    subida de paginador y buscador

--- Js/tienda.js
+++ Js/tienda.js
@@ -1,13 +1,76 @@
-getProductos(showProducto);
+const PAGE_SIZE = 1;
+
+getProductos(showProducto, 1, PAGE_SIZE, "", "Title");
+
+let listProductos = {};
+
+
+
+const txtQuery = document.getElementById("txt_query");
+txtQuery.addEventListener("input", (evt) => {
+    console.log("Botón de siguiente clickeado");
+    getProductos(showProducto, 1, PAGE_SIZE, txtQuery.value, "Title");
+})
+
+
+
+  
+
+const firstProducto = document.getElementById("first_producto");
+firstProducto.addEventListener("click", (evt) => {
+    getProductos(showProducto, 1, PAGE_SIZE, txtQuery.value, "Title");
+});
+
+
+
+const prevProducto = document.getElementById("prev_producto");
+prevProducto.addEventListener("click", (evt) => {
+    getProductos(showProducto, listProductos.prevPage, PAGE_SIZE, txtQuery.value, "Title");
+});
+
+
+
+const nextProducto = document.getElementById("next_producto");
+nextProducto.addEventListener("click", (evt) => {
+    getProductos(showProducto, listProductos.nextPage, PAGE_SIZE, txtQuery.value, "Title");
+});
+
+
+
+
+const lastProducto = document.getElementById("last_producto");
+lastProducto.addEventListener("click", (evt) => {
+    getProductos(showProducto, listProductos.totalPage, PAGE_SIZE, txtQuery.value, "Title");
+});
+
+
+
+
+
+
 
 function showProducto(productos){
-    const listaProducto = document.getElementById("products");
+
+    listProductos = productos;
+
+    const pageProducto = document.getElementById("page_producto");
+    pageProducto.innerText = listProductos.page;
+
+    nextProducto.style.display = listProductos.hasNextPage ? "block" : "none";
+    lastProducto.style.display = listProductos.hasNextPage ? "block" : "none";
+    prevProducto.style.display = listProductos.hasPrevPage ? "block" : "none";
+    firstProducto.style.display = listProductos.hasPrevPage ? "block" : "none";
+
+    const listaProducto = document.getElementById("producto_list");
+
+    listaProducto.innerHTML = "";
     for(const producto of productos.list) {
         const product = new Producto(producto);
         const template = getTemplate(product);
         listaProducto.append(template);
     }
 }
+
 
 function getTemplate(producto) {
     const div = document.createElement("div");
@@ -34,3 +97,6 @@ div.innerHTML =  `
 </div>`;
 return div
 }
+
+
+
